944d8a4a2f9dc033c9a2bdbe7d6665c3e072f424
alexmrt25 <alexandre.mourot@epfl.ch>
2022-11-14 20:40:36 +0100

    tests

---
+++ src/UniswapV3Pool.sol
@@ -0,0 +1,51 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// src/lib/Tick.sol
+library Tick {
+    struct Info {
+        bool initialized;
+        uint128 liquidity;
+    }
+    ...
+}
+
+// src/lib/Position.sol
+library Position {
+    struct Info {
+        uint128 liquidity;
+    }
+    ...
+}
+
+// src/UniswapV3Pool.sol
+contract UniswapV3Pool {
+    using Tick for mapping(int24 => Tick.Info);
+    using Position for mapping(bytes32 => Position.Info);
+    using Position for Position.Info;
+
+    int24 internal constant MIN_TICK = -887272;
+    int24 internal constant MAX_TICK = -MIN_TICK;
+
+    // Pool tokens, immutable
+    address public immutable token0;
+    address public immutable token1;
+
+    // Packing variables that are read together
+    struct Slot0 {
+        // Current sqrt(P)
+        uint160 sqrtPriceX96;
+        // Current tick
+        int24 tick;
+    }
+    Slot0 public slot0;
+
+    // Amount of liquidity, L.
+    uint128 public liquidity;
+
+    // Ticks info
+    mapping(int24 => Tick.Info) public ticks;
+    // Positions info
+    mapping(bytes32 => Position.Info) public positions;
+
+    ...
